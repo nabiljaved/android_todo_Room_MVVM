9ff82653a3b2d39819771a9212a0c2711ba0880e
nabiljaved <nabeeljaved2016@gmail.com>
2021-11-18 18:21:26 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Basic TODO APP MVVM ROOM WITH SWIPE EDIT AND DELETE
+This is a simple to do app with swipe functionality to update or delete item 
+MVVM Architecture is used Live Data and Advanced Form of SQLITE with ROOM Data Base
+
+# Ask a question please contact?
+
+If you have any query please contact at nabeeljaved2029@gmail.com
+
+## Screenshot
+
+![Android Code](/screenshots/main.png)
+![Android Code](/screenshots/delete.png)
+![Android Code](/screenshots/edit.png)
+![Android Code](/screenshots/confirmdelete.png)
---
+++ app/src/main/java/com/androidarchitecture/learn/noteapplication/NoteViewModel.java
@@ -0,0 +1,111 @@
+package com.androidarchitecture.learn.noteapplication;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.util.List;
+
+public class NoteViewModel extends AndroidViewModel {
+
+    private String TAG = this.getClass().getSimpleName();
+    private NoteDao noteDao;
+    private NoteRoomDatabase noteDB;
+    private LiveData<List<Note>> mAllNotes;
+
+    public NoteViewModel(Application application) {
+        super(application);
+
+        noteDB = NoteRoomDatabase.getDatabase(application);
+        noteDao = noteDB.noteDao();   //notedao = db.notedao();
+        mAllNotes = noteDao.getAllNotes();
+    }
+
+    //insert
+    public void insert(Note note)
+    {
+        new InsertAsyncTask(noteDao).execute(note);
+    }
+
+    LiveData<List<Note>> getAllNotes() {
+        return mAllNotes;
+    }
+
+    //update
+    public void update(Note note) {
+        new UpdateAsyncTask(noteDao).execute(note);
+    }
+
+    //delete
+    public void delete(Note note) {
+        new DeleteAsyncTask(noteDao).execute(note);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.i(TAG, "ViewModel Destroyed");
+    }
+
+
+
+    private class OperationsAsyncTask extends AsyncTask<Note, Void, Void> {
+
+        //this is NateDao object
+        NoteDao mAsyncTaskDao;
+
+        OperationsAsyncTask(NoteDao dao) {
+            //initialize dao class
+            this.mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            return null;
+        }
+    }
+
+    private class InsertAsyncTask extends OperationsAsyncTask {
+
+        //receive Natdao reference object and execute superclass constructor
+        InsertAsyncTask(NoteDao mNoteDao) {
+            super(mNoteDao);
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            mAsyncTaskDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+    private class UpdateAsyncTask extends OperationsAsyncTask {
+
+        //receive Natdao reference object and execute superclass constructor
+        UpdateAsyncTask(NoteDao noteDao) {
+            super(noteDao);
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            mAsyncTaskDao.update(notes[0]);
+            return null;
+        }
+    }
+
+    private class DeleteAsyncTask extends OperationsAsyncTask {
+
+        public DeleteAsyncTask(NoteDao noteDao) {
+            super(noteDao);
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            mAsyncTaskDao.delete(notes[0]);
+            return null;
+        }
+    }
+}
